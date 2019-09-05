1b2892222a5f9796bd3c66928992cf6fdf4e0d6e
Kenda Blair <canikostar99@gmail.com>
2019-09-05 13:30:00 -0400

    Fixed the double println

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -5,7 +5,6 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Dolly");
-		System.out.println("Hello Dolly from Eclipse Again.");
 		System.out.println("Hello Dolly from Eclipse Again");
 	}
 
